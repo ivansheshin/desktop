79624e1c5c5fe5b66a9cae3e73c16a2a7e6cfcc8
ivansheshin <ivashesh@gmail.com>
2025-03-06 20:07:59 +0300

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <header>My Header</header>
+    <slot />
+    <footer>My Footer</footer>
+  </div>
+</template>
+
---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    ыфваыфаыы
+  </div>
+</template>
+
+<!--<style scoped>-->
+<!--.home {-->
+<!--  background-image: url("/carpet.webp");-->
+<!--}-->
+<!--</style>-->
